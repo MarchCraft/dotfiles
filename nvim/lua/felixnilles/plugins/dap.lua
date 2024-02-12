6969e33a89d68f4ee9d670380162479a303b4b35
Felix Nilles <felix@dienilles.de>
2024-02-12 20:48:34 +0100

    jdtls

--- nvim/lua/felixnilles/plugins/dap.lua
+++ nvim/lua/felixnilles/plugins/dap.lua
@@ -14,12 +14,6 @@ return {
             dap.listeners.before.launch.dapui_config = function()
                 dapui.open()
             end
-            dap.listeners.before.event_terminated.dapui_config = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited.dapui_config = function()
-                dapui.close()
-            end
             vim.keymap.set("n", "<Leader>dt", ':DapToggleBreakpoint<CR>')
             vim.keymap.set("n", "<Leader>dx", ':DapTerminate<CR>')
             vim.keymap.set("n", "<Leader>do", ':DapStepOver<CR>')
