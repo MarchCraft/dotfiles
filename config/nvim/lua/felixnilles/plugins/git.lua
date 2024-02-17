88f3bc6d1d6ccb7dc7c2190940170d10a4fa339c
Felix Nilles <felix@dienilles.de>
2024-02-17 21:43:57 +0100

    update

--- config/nvim/lua/felixnilles/plugins/git.lua
+++ config/nvim/lua/felixnilles/plugins/git.lua
@@ -1,9 +1,17 @@
 return {
-    'lewis6991/gitsigns.nvim',
-    requires = {
-        'nvim-lua/plenary.nvim'
+    {
+        'lewis6991/gitsigns.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim'
+        },
+        config = function()
+            require('gitsigns').setup()
+        end
     },
-    config = function()
-        require('gitsigns').setup()
-    end
+    {
+        "kdheepak/lazygit.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
 }
