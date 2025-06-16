26e484c8f43c147ef9275845820018e86b97b882
Felix Nilles <felix@dienilles.de>
2025-06-16 18:07:32 +0200

    change theme

--- nixos/share/nixos.nix
+++ nixos/share/nixos.nix
@@ -1,7 +1,7 @@
 { pkgs, outputs, ... }:
 {
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
   stylix.image = pkgs.fetchurl {
     url = "https://github.com/NixOS/nixos-artwork/blob/master/wallpapers/nixos-wallpaper-catppuccin-mocha.png?raw=true";
     sha256 = "sha256-fmKFYw2gYAYFjOv4lr8IkXPtZfE1+88yKQ4vjEcax1s=";
